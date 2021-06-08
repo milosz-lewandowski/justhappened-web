032790d150c13887f4ef802d5e99f0b01131cd0b
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-08 22:09:32 +0200

    delete unnecesary links

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, } from "react-bootstrap";
-import {LaunchTechDetailsModal} from "../TechnicalDetails";
+import {Navbar, Nav, NavDropdown, Dropdown,} from "react-bootstrap";
 
 
 export class NavigationBar extends React.Component{
@@ -8,22 +7,14 @@ export class NavigationBar extends React.Component{
         return(
             <Navbar bg="light" expand="lg" >
                 <Nav className="justify-content-center">
-                        <NavDropdown title="About me" id="basic-nav-dropdown">
-                            {/*<NavDropdown.Item href="https://miloszlewandowski.pl/justhappened">JustHappened description</NavDropdown.Item>*/}
-                            <NavDropdown.Item href="#link" >My resume</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#link">JP2 GMD Memes [HOT]</NavDropdown.Item>
-                            {/*<NavDropdown.Item href="https://miloszlewandowski.pl/index">Previous static page</NavDropdown.Item>*/}
-                            {/*<NavDropdown.Item href="https://miloszlewandowski.pl/therapists">Rest API</NavDropdown.Item>*/}
-                            {/*<NavDropdown.Item onClick={LaunchTechDetailsModal}>Technical details</NavDropdown.Item>*/}
+                        <NavDropdown title="resumes" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf" >polish</NavDropdown.Item>
+                            <NavDropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf" >english</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    {/*<Nav.Link href="#link" >My resume</Nav.Link>*/}
-                    {/*<Nav.Link href="#link" >My resume</Nav.Link>*/}
                     <Nav.Link href="https://miloszlewandowski.pl/therapists">Rest API</Nav.Link>
                     <Nav.Link href="https://miloszlewandowski.pl/justhappened">JustHappened</Nav.Link>
                     <Nav.Link href="https://miloszlewandowski.pl/index">miloszlewandowski.pl</Nav.Link>
-                    {/*<Nav.Link href="#home">About me</Nav.Link>*/}
             </Navbar>
         )
     }
