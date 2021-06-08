c953396a0e21e1c5e8b6318cfef1c57ab5d4e52f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-08 22:06:27 +0200

    Dropdown menu with resumes

---
+++ src/components/ResumeDropdown.js
@@ -0,0 +1,19 @@
+import {Dropdown} from "react-bootstrap";
+import React from "react";
+import {ReactComponent} from "*.svg";
+
+export class ResumeDropdown extends ReactComponent {
+    render() {
+        return (
+            <Dropdown>
+                <Dropdown.Toggle variant={"success"} id="dropdown-basic">
+                    resume
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf">english</Dropdown.Item>
+                    <Dropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        );
+    }
+}
