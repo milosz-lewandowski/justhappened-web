4eec00b288f717291d19402b9f7a1276d2d744ac
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-09 22:21:07 +0200

    diplomas and certificates tab

---
+++ src/components/contentTabs/Diplomas.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export class Diplomas extends React.Component{
+    render() {
+        return(
+            <ul>
+                <li>
+                    Java Foundations | Certified Junior Associate | Oracle | 2020
+                    <br/>
+                    <a href="https://www.youracclaim.com/users/milosz-lewandowski/badges">See certificate</a>
+                </li>
+                <li>
+                    Java Developer Web | IT Bootcamp | CodersLab | 2018
+                </li>
+                <li>
+                    Physiotherapy | Bachelor’s degree | Social Academy of Sciences
+                    in Poland | 2016 - 2019
+                </li>
+                <li>
+                    British English (B2) | First Certificate in English (FCE)
+                </li>
+            </ul>
+        )
+    }
+}
