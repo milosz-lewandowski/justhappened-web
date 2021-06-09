73e5c2773046ab88b95cc94a1bbbc890eec6da92
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-09 22:27:01 +0200

    About Me component

---
+++ src/components/contentTabs/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class AboutMe extends React.Component {
+    render() {
+        return (
+            <p>As a software developer I constantly work on
+                gaining knowledge and skills in the field of
+                programming and application design. My
+                motto is that a sharp mind comes along with a
+                healthy lifestyle. That is why I am both a
+                sportsman and a developer. I am full of energy,
+                always ready to face new challenges. I enjoy
+                working with people. My ideal job is one that
+                connects teamwork and challenging tasks. My
+                working style: creativity and cooperation to
+                meet the project goals.
+            </p>
+        )
+    }
+}
