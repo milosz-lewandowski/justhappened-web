cb6efe3a1e7797e5b14b5873c145293c215d76c5
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-09 22:20:47 +0200

    moved to game directory

--- src/components/game/GameSection.js
+++ src/components/game/GameSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
-import {Game} from "./game/Game";
+import {Game} from "./Game";
 
 export class GameSection extends React.Component{
     render() {
