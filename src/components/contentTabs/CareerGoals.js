716a877ae711ef298243eb7182b1ce9430c0d8cb
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-09 22:27:17 +0200

    Career Goals component

---
+++ src/components/contentTabs/CareerGoals.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class CareerGoals extends React.Component {
+    render() {
+        return (
+            <p>
+                Gain practical experience in Java, Spring Boot and Docker. I also want to evolve
+                my skills & knowledge in mobile applications development with Flutter and Dart.
+                Currently I am also working on preparation to earn higher levels of Oracle Java
+                certification, especially the newest versions of Java.
+            </p>
+        )
+    }
+}
