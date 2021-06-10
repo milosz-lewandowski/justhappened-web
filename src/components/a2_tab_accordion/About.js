2be6448f860b40a9a86f52958acf7b462a042c14
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-10 06:57:46 +0200

    "About..." tab accordion

---
+++ src/components/a2_tab_accordion/About.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {Accordion, Button, Card, Col, Image, ListGroup, Row} from "react-bootstrap";
+import {PhoneItem} from "./a3_accordion_card/a4_card_item/PhoneItem";
+import {MailItem} from "./a3_accordion_card/a4_card_item/MailItem";
+import {GithubItem} from "./a3_accordion_card/a4_card_item/GithubItem";
+import {LinkedinItem} from "./a3_accordion_card/a4_card_item/LinkedinItem";
+import {CredlyProfileItem} from "./a3_accordion_card/a4_card_item/CredlyProfileItem";
+import {AboutThisPage} from "./a3_accordion_card/AboutThisPage";
+import {AboutThisGuy} from "./a3_accordion_card/AboutThisGuy";
+import {AboutContactToHim} from "./a3_accordion_card/AboutContactToHim";
+import {OtherWebsitesHeLay} from "./a3_accordion_card/OtherWebsitesHeLay";
+
+export class About extends React.Component {
+    render() {
+        return (
+            <Accordion defaultActiveKey="4">
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="text" eventKey="1">
+                            ...this page
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        <Card.Body>
+                            <AboutThisPage/>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="text" eventKey="2">
+                            ...this guy
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="2">
+                        <Card.Body>
+                            <AboutThisGuy/>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="text" eventKey="3">
+                            ...contact to him
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="3">
+                        <Card.Body as={ListGroup} fluid>
+                            {/*<AboutContactToHim/>*/}
+                            <ListGroup.Item fluid>
+                                <PhoneItem/>
+                            </ListGroup.Item>
+                            <ListGroup.Item fluid>
+                                <MailItem/>
+                            </ListGroup.Item>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="text" eventKey="4">
+                            ...other websites reply
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="4">
+                        <Card.Body as={ListGroup}>
+                            <ListGroup.Item>
+                                <GithubItem/>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <LinkedinItem/>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <CredlyProfileItem/>
+                            </ListGroup.Item>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+        )
+    }
+}
