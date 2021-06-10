78af24e6c0d235fb0a65932def7074fcb140f8ec
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-10 07:25:28 +0200

    AboutThisGuy.js

--- src/components/a2_tab_accordion/a3_accordion_card/AboutThisGuy.js
+++ src/components/a2_tab_accordion/a3_accordion_card/AboutThisGuy.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {AboutMe} from "../../a3_contentTabs/AboutMe";
 
 export class AboutThisGuy extends React.Component {
     render() {
         return (
-            <div></div> //TODO: AboutThisGuy
+            <div>
+                <AboutMe/>
+            </div> //TODO: AboutThisGuy
         )
     }
 }
