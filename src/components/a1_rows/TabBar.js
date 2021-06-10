aba9ad78bff7260fa76db750f54a8456b1e55560
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-10 07:46:36 +0200

    move TabBar

---
+++ src/components/a1_rows/TabBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Card, CardColumns, Dropdown, Tab, Tabs} from "react-bootstrap";
+import {About} from "../a2_tab_accordion/About";
+import {IT_tab} from "../a2_tab_accordion/IT_tab";
+import {SectionsContainer} from "../a2_tab_accordion/SectionsContainer";
+import {ResumeDropdown} from "../a2_tab_accordion/a3_entertainment_accordion_card/ResumeDropdown";
+
+export class TabBar extends React.Component{
+    render() {
+        return(
+            <Tabs fill defaultActiveKey="about">
+                <Tab eventKey="about" title="About...">
+                    <About/>
+                </Tab>
+                <Tab eventKey="itlife" title="IT life">
+                    <IT_tab/>
+                </Tab>
+                <Tab eventKey="ent" title="Entertainment">
+                    <SectionsContainer/>
+                </Tab>
+                <Tab eventKey="resume" title="Extremly interesting resume">
+                    <ResumeDropdown/>
+                    {/*<Dropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf">english</Dropdown.Item>*/}
+                    {/*<Dropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish</Dropdown.Item>*/}
+                </Tab>
+                {/*<Tab title="Diplomas & certificates" eventKey="diplomas" >*/}
+                {/*    <Diplomas/>*/}
+                {/*</Tab>*/}
+            </Tabs>
+        )
+    }
+}
