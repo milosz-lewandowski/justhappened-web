47cbb3fb7cd1d42d3a944dbbb8a24ad1e0c95097
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-10 07:54:13 +0200

    Move NavigationBar.js

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,21 +1,44 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Dropdown,} from "react-bootstrap";
+import {Navbar, Nav, NavDropdown, Dropdown, Form, FormControl, Button,} from "react-bootstrap";
 
 
-export class NavigationBar extends React.Component{
+export class NavigationBar extends React.Component {
     render() {
-        return(
-            <Navbar bg="light" expand="lg" >
-                <Nav className="justify-content-center">
-                        <NavDropdown title="resumes" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf" >polish</NavDropdown.Item>
-                            <NavDropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf" >english</NavDropdown.Item>
+        return (
+            <Navbar bg="light" expand="lg" as="ToggleButton">
+                {/*<Navbar.Brand href="#home">Junior Dev</Navbar.Brand>*/}
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav fill variant="tabs">
+                        <Nav.Item>
+                            <Nav.Link>About me</Nav.Link>
+                        </Nav.Item>
+                        <NavDropdown title="Get resume" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf">english
+                                resume</NavDropdown.Item>
+                            <NavDropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish
+                                resume</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Nav.Link href="https://miloszlewandowski.pl/therapists">Rest API</Nav.Link>
-                    <Nav.Link href="https://miloszlewandowski.pl/justhappened">JustHappened</Nav.Link>
-                    <Nav.Link href="https://miloszlewandowski.pl/index">miloszlewandowski.pl</Nav.Link>
+
+                </Navbar.Collapse>
             </Navbar>
-        )
+            // <Navbar bg="light" expand="lg">
+            //     {/*<Navbar.Brand as="toggle">Get resume</Navbar.Brand>*/}
+            //     <Navbar.Toggle />
+            //     <Navbar.Collapse >
+            //     <Nav>
+            //         <Nav.Item>
+            //             <Nav.Link >english</Nav.Link>
+            //         </Nav.Item>
+            //         <Nav.Item>
+            //             <Nav.Link href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish</Nav.Link>
+            //         </Nav.Item>
+            //         <NavDropdown title="resumes" id="basic-nav-dropdown">
+            //         </NavDropdown>
+            //     </Nav>
+            //     </Navbar.Collapse>
+            // </Navbar>
+        );
     }
 }
