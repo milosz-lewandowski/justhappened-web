3e98bb14137ec9459e86fd6aebda01a70c6cb80f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:35:04 +0200

    LinkedinItem.js

---
+++ src/components/a2_tab_accordion/a3_accordion_card/a4_card_item/LinkedinItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Card, Col, Image, Row} from "react-bootstrap";
+
+export class LinkedinItem extends React.Component {
+    render() {
+        return (
+            <Row className="align-middle"
+                // className="justify-content-md-center"
+            >
+                <Image
+                    src="miniatures/linkedin_round.png"
+                    height="40"
+                    width="40"
+                    alt="linkedin profile"
+                />
+                <Col
+                // className="align-middle"
+                > https://www.linkedin.com/in/miloszlewandowski</Col>
+            </Row>
+        )
+    }
+}
