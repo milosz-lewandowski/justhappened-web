c9baab88da3f9dbfb8b4a3637027a7afb9f2c00c
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:33:18 +0200

    about this page card

--- src/components/a2_tab_accordion/a3_accordion_card/AboutThisPage.js
+++ src/components/a2_tab_accordion/a3_accordion_card/AboutThisPage.js
@@ -3,7 +3,10 @@ import React from "react";
 export class AboutThisPage extends React.Component {
     render() {
         return (
-            <div></div> //TODO: AboutThisPage
+            <div>
+                <p>I started my adventure with React-Bootstrap recently, which is the reason of this page condition
+                </p>
+            </div> //TODO: AboutThisPage
         )
     }
 }
