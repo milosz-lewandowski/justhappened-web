a74aa2464015c0b26cc84ccf2b1b0fc9fe01cf75
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:34:33 +0200

    JustHappened description card

---
+++ src/components/a2_tab_accordion/a3_entertainment_accordion_card/Jhdesc.js
@@ -0,0 +1,108 @@
+import React from "react";
+
+export class Jhdesc extends React.Component {
+    render() {
+        return (
+            <div>
+                <h5>Słowem wstępu:</h5>
+                <p>JustHappened docelowo ma być aplikacją stworzoną dla terapeutów.
+                    Terapeuta pracujacy z tą aplikacją będzie posiadał swój profil, oraz profile swoich pacjentów,
+                    z którymi co 1 lub 2 tygodnie na spotkaniu będzie omawiał i analizował zebrane dane.</p>
+                <p>Poniższy opis jest jednak przenaczony dla samego użytkownika. Aplikacja jak najbardziej będzie się
+                    nadawać do użytkowania jej samemu, jednakże owocne i skuteczne korzystanie z niej wymaga dość
+                    wysokiego poziomu samoświadomości i wiedzy.</p>
+                <p>Projekt jest w trakcie rozwoju, także chętnych do współpracy, do testów, czy po prostu
+                rozmowy na jej temat - serdecznie zapraszam do kontaktu!</p>
+                <br/>
+                <h1
+                    style={{textAlign: "center"}} >Just Happened</h1>
+                <h3>Główne cele:</h3>
+                <ul>
+                    <li>Motywowanie pacjenta do walki z nałogiem, lub
+                        zmiany zachowań
+                    </li>
+                    <li>Przewidywanie kiedy może nastąpić trudniejszy
+                        okres, gorszego samopoczucia, większego ryzyka niechcianego
+                        zachowania/aktywności
+                    </li>
+                    <li>Wspieranie pacjenta w chwilach kryzysowych
+                        poprzez wyświetlanie przypominajek (tekst, zdjęcia, film, audio), powodów dla
+                        których pacjent chce coś zmienić.
+                    </li>
+                    <li>Uświadomienie swoich potrzeb oraz sposobów ich
+                        zaspokajania (zarówno negatywnych jak i pozytywnych)
+                    </li>
+                    <li>Dostrzeżenie wyzwalaczy powodujących niechciane
+                        zachowania
+                    </li>
+                    <li>Uświadomienie sobie błędnych przekonań, z
+                        których wynika m.in. zaniżone poczucie własnej wartości
+                    </li>
+                    <li>Dostrzeżenie schematów myślenia i działania oraz
+                        świadome ich zmienianie
+                    </li>
+                </ul>
+                <h3>Slogan:</h3>
+
+                <p>Miałeś tego nie robić a znowu się zdarzyło? To już
+                    pięćdziesiąta wymówka wytłumaczenie czy powód i sam zauważasz że są to rzeczy
+                    coraz bardziej błahe? Dobra też to widzisz. Sytuacja wymyka się spod kontroli</p>
+
+                <h3>Wizja dla użytkownika:</h3>
+
+                <p>Skoro trafiłeś na tą aplikację to najprawdopodobniej chcesz
+                    zmienić coś w swoim funkcjonowaniu. Może to być uzależnienie. Może to być
+                    nieumiejętność panowania nad sobą. Może to być uciekanie od trudnych czy stresowych
+                    sytuacji, albo natręctwa takie jak obgryzanie paznokci, czy rozdrapywanie ran,
+                    wyciskanie pryszczy. Niezależnie od tego, czy to zawsze to samo zachowanie, czy
+                    też za każdym razem inne – problem się powtarza. </p>
+
+                <p>To czy znowu zrobisz coś czego nie chciałeś zależy od paru
+                    czynników. </p>
+
+                <p>Pierwszy to <b>wyzwalacz</b>, jakaś sytuacja, która powoduje
+                    że robisz to czego chcesz uniknąć. To może być zawsze to samo, lub za każdym
+                    razem coś innego (kłótnia, stres, samotność, nagła okazja)</p>
+
+                <p>Drugi to poziom twoich <b>zasobów wewnętrznych</b>. Tego,
+                    jak mocno jesteś zmotywowany, ile masz sił fizycznych i psychicznych, jak długo
+                    już wytrzymałeś bez wykonywania niechcianego zachowania, a zatem jak bardzo Cię
+                    do niego ciągnie.</p>
+
+                <p>Ten program to nie jest parę magicznych trików, które
+                    każdemu można wcisnąć tak samo. Korzystanie z tej aplikacji będzie wymagało od
+                    Ciebie dokładne przeanalizowanie swojego życia, swoich <b>potrzeb</b>, oraz
+                    sposobów ich zaspokajania, czyli <b>aktywności</b>. </p>
+
+                <p>Wiele osób nie jest do końca świadoma czego naprawdę
+                    potrzebują i ciągły dyskomfort spowodowany jakimiś brakami próbują zaspokajać w
+                    niezdrowe sposoby nie dające tak naprawdę ukojenia</p>
+
+                <p>Oparte na powiadomieniach systematyczne zapisywanie czy też
+                    notowanie swoich <b>myśli</b>, <b>emocji</b> i <b>samopoczucia</b>, pomoże ci
+                    zrozumieć co wpływa na poziom twoich <b>zasobów wewnętrznych</b> i pomoże
+                    funkcjonować w taki sposób, aby stale je zwiększać. Dodatkowo pomoże Ci
+                    dostrzec kiedy spadają (a u każdego to prędzej czy później nastąpi) i lepiej
+                    przygotować się na oparcie <b>wyzwalaczowi</b>. </p>
+
+                <p>Korzystanie z aplikacji pomoże Ci w pierwszej kolejności
+                    zrozumienie co dla Ciebie jest <b>wyzwalaczem </b>lub <b>wyzwalaczami</b>, oraz
+                    jak ich unikać. Nie zawsze się uda uniknąć wyzwalacza, ale wtedy to czy mu
+                    ulegniesz jest zależne od twoich <b>zasobów wewnętrznych</b> i umiejętności
+                    poradzenia sobie z trudną sytuacją w inny sposób, który opracujesz sam lub z
+                    terapeutą. </p>
+
+                <p>Dodatkowo w sytuacjach krytycznych zawsze będziesz mógł
+                    sięgnąć do zapisanych przez siebie <b>motywatorów (przypominajek)</b>, dla
+                    których chcesz coś zmienić. Przypomnisz sobie co tracisz przez niechciane
+                    zachowania i co chcesz osiągnąć dzięki ich zmianie.</p>
+
+                <p>Mimo wszystko zrobisz to czego chciałeś uniknąć, lub nawet
+                    nie spojrzysz do aplikacji? Trudno, zdarza się, każda zmiana wymaga czasu i
+                    pracy, a ani aplikacja, ani twój terapeuta Cię nie ocenia, tylko trzyma za
+                    Ciebie kciuki, żeby następnym razem poszło Ci lepiej.</p>
+
+            </div>
+        )
+    }
+}
