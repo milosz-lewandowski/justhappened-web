6ec1f49ba61030bb18f3937fa9fc8cdafb59a7f4
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:32:17 +0200

    tech details

--- src/components/a2_tab_accordion/a3_IT_accordion_card/TechnicalDetails.js
+++ src/components/a2_tab_accordion/a3_IT_accordion_card/TechnicalDetails.js
@@ -6,15 +6,36 @@ export class TechnicalDetails extends React.Component {
         return (
             <Row>
                 <Col>
+                    <p>Because my technologies are listed in my resume, which is available in “Extremely interesting
+                        resume” section I will shortly present my project’s architecture.</p>
                     <ul>
-                        <li>This page is hosted on AWS Amplify cloud hosting</li>
-                        <li>Frontend uses React and React-Bootstrap</li>
-                        <li/>
-                        <li>Backend is written in Spring Boot</li>
-                        <li>Hosted on OVH VPS</li>
-                        <li>As database I use PostgreSQL databases</li>
-                        <li>Both Spring Boot application and Database are run in Docker containers</li>
+                        <p>Back-end:
+                            <p/>
+                            <li>Hosting: VPS with Debian OS.</li>
+                            <li>All services are run in Docker containers.</li>
+                            <li>Db: PostgerSQL database.</li>
+                            <li>Rest API: Spring-Boot application.</li>
+                        </p>
+                        <p>Web server:
+                            <p/>
+                            <li>Node.js</li>
+                            <li>Proxy server: Caddy</li>
+                        </p>
+                        <p>
+                            Web:
+                            <p/>
+                            <li>React-Bootstrap</li>
+                        </p>
+                        <p>
+                            Mobile:
+                            <p/>
+                            <li>Flutter application (soon available online).</li>
+                            <li>I’m also developing compatible Wear OS software.</li>
+                        </p>
                     </ul>
+                    <p>All projects with docker-compose.yml file are available on my GitHub profile.</p>
+                    <p>I started my adventure with React-Bootstrap recently, which is the reason of this page condition
+                    </p>
                 </Col>
             </Row>
         )
