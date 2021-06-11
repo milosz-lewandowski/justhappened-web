f45812adc7b0359356fd28c8f30992ab332580d6
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:41:58 +0200

    my websites links card

---
+++ src/components/a2_tab_accordion/a3_accordion_card/OtherWebsitesHeLay.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {GithubItem} from "./a4_card_item/GithubItem";
+import {Card, ListGroupItem} from "react-bootstrap";
+
+export class OtherWebsitesHeLay extends React.Component {
+    render() {
+        return (
+            <div></div> //todo
+        )
+    }
+}
