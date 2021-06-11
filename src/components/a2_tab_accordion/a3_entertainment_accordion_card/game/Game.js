b76ca32ba344792c198cbf2aad5b47903068f46f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-11 17:34:08 +0200

    delete restart button

--- src/components/a2_tab_accordion/a3_entertainment_accordion_card/game/Game.js
+++ src/components/a2_tab_accordion/a3_entertainment_accordion_card/game/Game.js
@@ -11,14 +11,14 @@ export class Game extends React.Component {
                 <div className="game-board">
                     <Board />
                 </div>
-                    <Button
-                        className={"restart-game-button"}
-                    // onClick={this.state.restartGame}
-                    >Restart game</Button>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
+                {/*    <Button*/}
+                {/*        className={"restart-game-button"}*/}
+                {/*    // onClick={this.state.restartGame}*/}
+                {/*    >Restart game</Button>*/}
+                {/*<div className="game-info">*/}
+                {/*    <div>/!* status *!/</div>*/}
+                {/*    <ol>/!* TODO *!/</ol>*/}
+                {/*</div>*/}
             </div>
         );
     }
