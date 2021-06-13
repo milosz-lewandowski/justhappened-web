9fd55d23ccd2b7d214eea8a59885e93e5208c79f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-13 02:45:30 +0200

    delete EIR

--- src/components/a1_rows/TabBar.js
+++ src/components/a1_rows/TabBar.js
@@ -3,13 +3,13 @@ import {Card, CardColumns, Dropdown, Tab, Tabs} from "react-bootstrap";
 import {About} from "../a2_tab_accordion/About";
 import {IT_tab} from "../a2_tab_accordion/IT_tab";
 import {EntartainmentTab} from "../a2_tab_accordion/EntartainmentTab";
-import {ResumeDropdown} from "../a2_tab_accordion/a3_entertainment_accordion_card/ResumeDropdown";
+import {ResumeDropdown} from "../a2_tab_accordion/ResumeDropdown";
 
 export class TabBar extends React.Component{
     render() {
         return(
-            <Tabs fill defaultActiveKey="about">
-                <Tab eventKey="about" title="About...">
+            <Tabs fill defaultActiveKey="about" >
+                <Tab eventKey="about" title="About..." >
                     <About/>
                 </Tab>
                 <Tab eventKey="itlife" title="IT life">
@@ -18,11 +18,11 @@ export class TabBar extends React.Component{
                 <Tab eventKey="ent" title="Entertainment">
                     <EntartainmentTab/>
                 </Tab>
-                <Tab eventKey="resume" title="Extremly interesting resume">
-                    <ResumeDropdown/>
+                {/*<Tab eventKey="resume" title="Extremly interesting resume">*/}
+                {/*    <ResumeDropdown/>*/}
                     {/*<Dropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf">english</Dropdown.Item>*/}
                     {/*<Dropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish</Dropdown.Item>*/}
-                </Tab>
+                {/*</Tab>*/}
                 {/*<Tab title="Diplomas & certificates" eventKey="diplomas" >*/}
                 {/*    <Diplomas/>*/}
                 {/*</Tab>*/}
