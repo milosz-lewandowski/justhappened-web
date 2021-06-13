1e5a609fa993760e1637f84825ad2597c9ff95f8
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-13 02:44:42 +0200

    title.js add resume dropdown

--- src/components/a1_rows/Title.js
+++ src/components/a1_rows/Title.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Badge, Col, Container, Image, Row} from "react-bootstrap";
+import {ResumeDropdown} from "../a2_tab_accordion/ResumeDropdown";
 
 export class Title extends React.Component {
     render() {
@@ -21,6 +22,13 @@ export class Title extends React.Component {
                         <h4 align="flex-end">
                             <Badge  variant="warning">under development </Badge>
                         </h4>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <ResumeDropdown/>
+                        </Row>
+                        <Row></Row>
+                        <Row></Row>
                     </Col>
                 </Row>
             </Container>
