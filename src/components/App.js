9f6752bd0a3de7d1933d04545aa8cb7816756833
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-15 07:51:29 +0200

    rename entertainment tab to my_projects

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import {NavigationBar} from "./navigation/NavigationBar";
+import {Col, Container, Row} from "react-bootstrap";
 import {Title} from "./a1_rows/Title";
-import {EntartainmentTab} from "./a2_tab_accordion/EntartainmentTab";
 import {TabBar} from "./a1_rows/TabBar";
 
 export class App extends React.Component{
