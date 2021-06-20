c93ecf27c6cbfa4e84bb1f7ba049f9cff7e1c9bd
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-20 16:18:05 +0200

    add test containers

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import {Title} from "./a1_rows/Title";
 import {TabBar} from "./a1_rows/TabBar";
+import {TestBoard} from "./a1_rows/TestBoard";
+import {Title2} from "./a1_rows/Title2";
 
 export class App extends React.Component{
 
@@ -18,11 +20,15 @@ export class App extends React.Component{
                         <Title />
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
+                {/*<Row>*/}
+                {/*    <Col>*/}
                         <TabBar/>
-                    </Col>
+                    {/*</Col>*/}
+                {/*</Row>*/}
+                <Row>
+                    <Title2/>
                 </Row>
+                <TestBoard/>
             </Container>
         )
     }
