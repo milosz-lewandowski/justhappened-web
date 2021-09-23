2c556d0a5e18fb1d54420f86a4742fe6ce38e138
Miłosz Lewandowski <m.lewandowski@nebucode.com>
2021-09-23 06:57:36 +0200

    Resume files update

--- src/components/a2_tab_accordion/ResumeDropdown.js
+++ src/components/a2_tab_accordion/ResumeDropdown.js
@@ -9,8 +9,8 @@ export class ResumeDropdown extends React.Component {
                 {/*    resume*/}
                 {/*</Dropdown.Toggle>*/}
                 {/*<Dropdown.Menu>*/}
-                    <Dropdown.Item href="/resumes/en_java_dev_m_lewandowski_2021.pdf">english</Dropdown.Item>
-                    <Dropdown.Item href="/resumes/pl_java_dev_m_lewandowski_2021.pdf">polish</Dropdown.Item>
+                    <Dropdown.Item href="/resumes/resume_milosz_lewandowski_2021.pdf">english</Dropdown.Item>
+                    <Dropdown.Item href="/resumes/cv_milosz_lewandowski_2021.pdf">polish</Dropdown.Item>
                 {/*</Dropdown.Menu>*/}
             </DropdownButton>
         );
